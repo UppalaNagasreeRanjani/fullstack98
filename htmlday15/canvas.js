746c2ee868b74f20ef7d7f6fe1a9984cf3a2179f
sri <uppalanagasri98@gmail.com>
2020-01-17 17:47:03 +0530

    Day friday 17th

---
+++ htmlday15/canvas.js
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<HTML>
+<BODY>
+<CANVAS ID="myCanvas" width="300" height="300" style="border:1px solid
+black">
+</CANVAS>
+<SCRIPT>
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+ctx.rect(30,40,120,120);
+ctx.stroke();
+</SCRIPT>
+</BODY>
+</HTML> 
