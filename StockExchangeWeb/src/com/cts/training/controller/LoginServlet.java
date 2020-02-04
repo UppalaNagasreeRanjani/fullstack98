f2b5ccea81f0704add7e577d1e07340fab7ef01c
sri <uppalanagasri98@gmail.com>
2020-02-04 18:02:54 +0530

    day 4th feb

--- StockExchangeWeb/src/com/cts/training/controller/LoginServlet.java
+++ StockExchangeWeb/src/com/cts/training/controller/LoginServlet.java
@@ -1,5 +1,16 @@
 package com.cts.training.controller;
 
-public class LoginServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet   {
+	protected void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException{
+		PrintWriter out=response.getWriter();
+		String user=request.getParameter("user");
+		String pass=request.getParameter("pass");
+}
 }
